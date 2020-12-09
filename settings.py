12010b87bda8f6e2bba713e06fd029a30360974e
ckobayashi714 <ckobayashi@csu.fullerton.edu>
2020-12-08 18:44:56 -0800

    edits color

--- settings.py
+++ settings.py
@@ -20,7 +20,7 @@ milliseconds = 0
 
 # Colors
 black = pygame.Color(0, 0, 0)
-white = pygame.Color(255, 225, 225)
+white = pygame.Color(255, 255, 255)
 red = pygame.Color(255, 0, 0)
 yellow = pygame.Color(255, 255, 0)
 blue = pygame.Color(0, 0, 255)
